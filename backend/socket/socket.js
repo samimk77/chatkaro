90f7d871d48461c92b8926767767c1f67fee651f
Samim Kausar <samimkausar240@gmail.com>
2026-02-22 11:39:27 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://chat-application-samimkausar240-6708s-projects.vercel.app/"],
     methods: ["GET", "POST"],
   },
 });
